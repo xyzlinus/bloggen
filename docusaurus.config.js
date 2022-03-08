c6daa1d221e67ac2c5facd46d6c5fe006223dcd6
xyzlinus <52217114+xyzlinus@users.noreply.github.com>
2022-03-08 19:21:33 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Blog about me..',
+        title: 'My Blog about me...',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
